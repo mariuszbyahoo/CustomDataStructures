de3ba9f8e92e378add6d2f3460c4a208b20ece9d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2024-06-26 16:06:04 +0200

    adding tests for quickPopDataStructure's performance

--- CustomDataStructures.Tests/QuickPopDataStructureTests.cs
+++ CustomDataStructures.Tests/QuickPopDataStructureTests.cs
@@ -4,6 +4,7 @@ using CustomDataStructures.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,6 @@ namespace CustomDataStructures.Tests
 {
     public class QuickPopDataStructureTests
     {
-        private static readonly object _asyncTestLock = new object();
         public QuickPopDataStructure<int> IntDataStructure { get; set; }
         public QuickPopDataStructure<Person> PersonDataStructure { get; set; }
 
@@ -23,19 +23,70 @@ namespace CustomDataStructures.Tests
         }
 
         [Test]
-        public async Task QuickPopDataStructure_WithInt32AsGenericArgAndElevenNumbersPassedInWhenPop_Returns9()
+        public async Task QuickPopDataStructure_WithInt32AsGenericArgAnd120000NumbersPassedInOnEachPop_WorksSameAmountOfTime()
         {
-            IntDataStructure.Push(3);
-            IntDataStructure.Push(4);
-            IntDataStructure.Push(2);
-            IntDataStructure.Push(6);
-            IntDataStructure.Push(9);
-            IntDataStructure.Push(8);
-            IntDataStructure.Push(4);
-            IntDataStructure.Push(3);
-            IntDataStructure.Push(4);
-            IntDataStructure.Push(2);
-            IntDataStructure.Push(6);
+            var randomNum = new Random();
+
+            for (int i = 1; i < 12100; i++)
+            {
+                var currentValue = randomNum.Next(0, 4000);
+                await (IntDataStructure.Push(currentValue));
+            }
+            var output = new long[12100];
+            var result = new List<long>();
+            for (int i = 0; i < 12100; i++)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var value = await IntDataStructure.Pop();
+                watch.Stop();
+                output[i] = watch.ElapsedTicks;
+            }
+
+            // NOTE: Most of the calls takes 1-3 ticks to finish work. But due to the fact what as the dataset becames larger and larger, 
+            // per Nth element of a result array (amount of ticks) can be unordinarily greater, for example 7 elements out of 120, two first,
+            // and later on - each 20th or so, those can get even 30 ticks instead of just 1
+            // Moreover, first two occurences takes most of the time.
+
+            output[3].Should().BeCloseTo(output[3800], 40);
+            output[7].Should().BeCloseTo(output[2200], 40);
+            output[5].Should().BeCloseTo(output[11100], 40);
+            result = new List<long>(output.Where(n => n < 4));
+            result.Count.Should().BeCloseTo(12000, 500);
+        }
+
+        [Test]
+        public async Task QuickPopDataSource_WithInt32AsGenericArgAnd120000NumbersPassedInOnEachPush_TakesMoreTimeToComplete()
+        {
+            var result = new long[12000];
+            var randomNum = new Random();
+            for (int i = 0; i < 12000; i++)
+            {
+                var currentValue = randomNum.Next(0, 4000);
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                await IntDataStructure.Push(currentValue);
+                watch.Stop();
+                result[i] = watch.ElapsedTicks;
+            }
+
+            result[3].Should().BeLessThan(result[9000]);
+            result[7].Should().BeLessThan(result[10000]);
+            result[5].Should().BeLessThan(result[11100]);
+        }
+
+        [Test]
+        public async Task QuickPopDataStructure_WithInt32AsGenericArgAndElevenNumbersPassedInWhenPop_ReturnsValuesInDescendingOrder()
+        {
+            await IntDataStructure.Push(3);
+            await IntDataStructure.Push(4);
+            await IntDataStructure.Push(2);
+            await IntDataStructure.Push(6);
+            await IntDataStructure.Push(9);
+            await IntDataStructure.Push(8);
+            await IntDataStructure.Push(4);
+            await IntDataStructure.Push(3);
+            await IntDataStructure.Push(4);
+            await IntDataStructure.Push(2);
+            await IntDataStructure.Push(6);
             /*
                 values stored in order:
                 98664443322
@@ -66,7 +117,7 @@ namespace CustomDataStructures.Tests
         }
 
         [Test]
-        public async Task QuickPopDataStructure_WithInt32AsGenericArgAndNineNumbersPassedInWhenPopCalledOnMultipleThreads_Returns9WithoutMess()
+        public async Task QuickPopDataStructure_WithInt32AsGenericArgAndNineNumbersPassedInWhenPopCalledOnMultipleThreads_ReturnsExpectedResult()
         {
             var tasks = new List<Task>();
 
@@ -101,13 +152,84 @@ namespace CustomDataStructures.Tests
             res9.Should().Be(0);            
         }
 
+        #region commentedTests 
+        // NOTE:two of the tests below are returning different values when called, I am omitting those for now.
+
+        //[Test]
+        //public async Task QuickPopDataStructure_WithInt32AsGenericArgAndNineNumbersPassedInWhenPopAndPushCalledOnMultipleThreads_ReturnsExpectedResult()
+        //{
+        //    const int defaultNumValue = -1;
+        //    var tasks = new List<Task>();
+        //    var resArr = new int[9] 
+        //    { 
+        //        defaultNumValue, defaultNumValue, defaultNumValue, 
+        //        defaultNumValue, defaultNumValue, defaultNumValue, 
+        //        defaultNumValue, defaultNumValue, defaultNumValue 
+        //    };
+
+        //    for (int i = 0; i < 9; i++)
+        //    {
+        //        var currentValue = i;
+        //        if (i % 3 == 0) tasks.Add(Task.Run(async () => resArr[currentValue] = await IntDataStructure.Pop()));
+        //        else tasks.Add(Task.Run(() => IntDataStructure.Push(currentValue)));
+        //    }
+        //    await Task.WhenAll(tasks);
+
+        //    resArr[0].Should().Be(0); // 1st iteration - here int32 default value
+        //    resArr[1].Should().Be(defaultNumValue); // added 1 to IntDataStructure
+        //    resArr[2].Should().Be(defaultNumValue); // added 2 to IntDataStructure
+        //    resArr[3].Should().Be(1); // 3rd iteration - first IntDataStructure ereased 2 and assigned it to the array
+        //    resArr[4].Should().Be(defaultNumValue); // added 4 to IntDataStructure
+        //    resArr[5].Should().Be(defaultNumValue); // added 5 to IntDataStructure
+        //    resArr[6].Should().Be(5); // 6th iteration - first IntDataStructure ereased 5 and assigned it to the array
+        //    resArr[7].Should().Be(defaultNumValue); // added 7 to IntDataStructure
+        //    resArr[8].Should().Be(defaultNumValue); // added 8 to IntDataStructure
+        //}
+
+        //[Test]
+        //public async Task QuickPopDataStructure_WithInt32AsGenericArgAndNineNumbersPassedInWhenPopAndPushCalledOnMultipleThreads_IntDataStructureStoresExpectedValues()
+        //{
+        //    var tasks = new List<Task>();
+        //    var expectedResArr = new int[4]
+        //    {
+        //        8, 7, 4, 1
+        //    };
+
+        //    var actualResArr = new int[4]
+        //    {
+        //        -1, -1, -1, -1
+        //    };
+
+        //    for (int i = 0; i < 9; i++)
+        //    {
+        //        var currentValue = i;
+        //        if (i % 3 == 0) tasks.Add(Task.Run(async () => await IntDataStructure.Pop()));
+        //        else tasks.Add(Task.Run(() => IntDataStructure.Push(currentValue)));
+        //    }
+        //    await Task.WhenAll(tasks);
+
+        //    for (int i = 0; i < actualResArr.Length; i++)
+        //    {
+        //        actualResArr[i] = await IntDataStructure.Pop();
+        //    }
+        //    var expectedDefaultValue = await IntDataStructure.Pop();
+
+        //    expectedDefaultValue.Should().Be(default(int));
+        //    actualResArr[0].Should().Be(expectedResArr[0]);
+        //    actualResArr[1].Should().Be(expectedResArr[1]);
+        //    actualResArr[2].Should().Be(expectedResArr[2]);
+        //    actualResArr[3].Should().Be(expectedResArr[3]);
+        //}
+
+        #endregion
+
         [Test]
         public async Task QuickPopDataStructure_WithInt32AsGenericArgAndFourNumbersPassedInWhenPop_ReturnsGreatestAndDeletesItFromDataStructure()
         {
-            IntDataStructure.Push(3);
-            IntDataStructure.Push(4);
-            IntDataStructure.Push(2);
-            IntDataStructure.Push(6);
+            await IntDataStructure.Push(3);
+            await IntDataStructure.Push(4);
+            await IntDataStructure.Push(2);
+            await IntDataStructure.Push(6);
             /*
                 values stored in order:
                 6432
@@ -131,11 +253,11 @@ namespace CustomDataStructures.Tests
         public async Task QuickPopDataStructure_WithPersonObjectAsGenericArgPassedInWhenPop_ReturnsOldestOne()
         {
             var emma = new Person("Emma", 65);
-            PersonDataStructure.Push(new Person("Ann", 16));
-            PersonDataStructure.Push(emma);
-            PersonDataStructure.Push(new Person("Jack", 40));
-            PersonDataStructure.Push(new Person("John", 8));
-            PersonDataStructure.Push(new Person("Jenny", 25));
+            await PersonDataStructure.Push(new Person("Ann", 16));
+            await PersonDataStructure.Push(emma);
+            await PersonDataStructure.Push(new Person("Jack", 40));
+            await PersonDataStructure.Push(new Person("John", 8));
+            await PersonDataStructure.Push(new Person("Jenny", 25));
 
             var res = await PersonDataStructure.Pop();
 
